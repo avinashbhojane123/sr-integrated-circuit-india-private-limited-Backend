9679c903c48ce0646b9c94354d515cd5466e83ac
Avinash Bhojane <140709275+avinashbhojane123@users.noreply.github.com>
2026-03-10 10:49:12 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,25 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+
+//   const app = await NestFactory.create(AppModule);
+
+//   // Enable CORS for frontend
+//   app.enableCors({
+//     origin: 'http://localhost:5173',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   });
+
+//   app.useGlobalPipes(new ValidationPipe());
+
+//   await app.listen(3000);
+// }
+
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -6,16 +28,22 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS for frontend
+  // Enable CORS globally
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
+  // Global validation
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  // Render requires dynamic port
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  console.log(`Server running on port ${port}`);
 }
 
 bootstrap();
